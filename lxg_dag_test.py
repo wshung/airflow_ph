24b83131e1d304a8b09320e7bd67150d63ab4dc1
Weisheng <paulhung49@gmail.com>
2024-07-29 15:23:35 +0800

    Update lxg_dag_test.py
    revised error

--- lxg_dag_test.py
+++ lxg_dag_test.py
@@ -1,8 +1,9 @@
 from datetime import datetime, timedelta
-from airflow import DAG
+# from airflow import DAG
+from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-
+from airflow.models import Variable, XCom
 
 default_args = {
     'owner': 'airflow',
@@ -15,23 +16,18 @@ default_args = {
 @dag(
     dag_id="lxg_dag_test",
     default_args=default_args,
-    start_date=datetime(2024, 6, 14,)
-    schedule_interval=None,
+    start_date=datetime(2024, 7, 22),
+    schedule_interval='@once',
     catchup=False,
-    tags=['lxg_tag']
+    tags=['lxg_tag_test']
     )
-
 def bash_dag():
+    
     task_1=BashOperator(
-        task_id='lxg_flow_02', 
-        bash_command='python3 /home/eileen_liao_yulon_group_com/airflow/dags/luxgen/flow_02.py',
-         )
-
-    task_2=BashOperator(
-        task_id='lxg_flow_03', 
-        bash_command='python3 /home/eileen_liao_yulon_group_com/airflow/dags/luxgen/flow_03.py',
+        task_id='lxg_flow_04', 
+        bash_command='python3 /home/eileen_liao_yulon_group_com/airflow/dags/test/flow_04.py',
          )
     
-    task_1>>task_2
+    task_1
 
 run_dag = bash_dag()
