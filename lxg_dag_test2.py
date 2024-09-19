1889571ebf4a6c311ec75d8508181afef45c5c90
Weisheng <paulhung49@gmail.com>
2024-09-19 09:35:54 +0800

    Update lxg_dag_test2.py
    use bashoperator

--- lxg_dag_test2.py
+++ lxg_dag_test2.py
@@ -1,10 +1,9 @@
+
 from datetime import datetime, timedelta
 # from airflow import DAG
 from airflow.decorators import dag
-from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from test.airflow_ph.luxgen import flow_04
 from airflow.models import Variable, XCom
 
 default_args = {
@@ -12,7 +11,7 @@ default_args = {
     'max_active_runs': 1,
     'email_on_retry': False,
     'retries': 0,
-    'retry_delay': timedelta(minutes=5)
+    'retry_delay': timedelta(minutes=5),
 }
 
 @dag(
@@ -25,9 +24,9 @@ default_args = {
     )
 def bash_dag():
     
-    task_1=PythonOperator(
+    task_1=BashOperator(
         task_id='lxg_flow_04', 
-        python_callable=flow_04.lxg_activity_task
+        bash_command='python3 /home/eileen_liao_yulon_group_com/airflow/dags/test/flow_04.py',
          )
     
     task_1
