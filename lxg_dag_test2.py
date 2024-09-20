2a7782639ac427e7112800c6c54fb08a846f0f12
Weisheng <paulhung49@gmail.com>
2024-09-20 16:17:54 +0800

    Update lxg_dag_test2.py
    adjust code

--- lxg_dag_test2.py
+++ lxg_dag_test2.py
@@ -9,26 +9,22 @@ from airflow.models import Variable, XCom
 default_args = {
     'owner': 'airflow',
     'max_active_runs': 1,
+    'start_date': datetime(2024, 7, 22),
     'email_on_retry': False,
     'retries': 0,
     'retry_delay': timedelta(minutes=5),
 }
 
-@dag(
+dag = dag(
     dag_id="lxg_dag_test",
     default_args=default_args,
-    start_date=datetime(2024, 7, 22),
     schedule_interval='@once',
-    catchup=False,
-    tags=['lxg_tag_test']
-    )
-def bash_dag():
-    
-    task_1=BashOperator(
-        task_id='lxg_flow_04', 
-        bash_command='python /home/eileen_liao_yulon_group_com/airflow/dags/test/airflow_ph/luxgen/flow_04.py',
-         )
-    
-    task_1
+)
 
-run_dag = bash_dag()
+lxg_flow_04=BashOperator(
+    task_id='lxg_flow_04', 
+    bash_command='python /home/eileen_liao_yulon_group_com/airflow/dags/test/airflow_ph/luxgen/flow_04.py',
+    dag=dag,
+)
+
+lxg_flow_04
